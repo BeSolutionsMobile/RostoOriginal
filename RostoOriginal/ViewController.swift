2d47f607e719f3df077c9f3cb7e404f9976723f6
merovic <amirahmedmorsy@yahoo.com>
2019-09-18 13:07:58 +0200

    Initial Commit

---
+++ RostoOriginal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RostoOriginal
+//
+//  Created by Apple on 9/18/19.
+//  Copyright © 2019 besolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RostoOriginalTests/RostoOriginalTests.swift
@@ -0,0 +1,34 @@
+//
+//  RostoOriginalTests.swift
+//  RostoOriginalTests
+//
+//  Created by Apple on 9/18/19.
+//  Copyright © 2019 besolutions. All rights reserved.
+//
+
+import XCTest
+@testable import RostoOriginal
+
+class RostoOriginalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
